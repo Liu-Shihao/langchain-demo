4f20bb311d158a8d02475c9db1e7bb152917542a
LiuShihao <liush99@foxmail.com>
2024-06-24 16:50:18 +0800

    gridsearchcv

--- src/spacy/test.py
+++ src/spacy/test.py
@@ -8,19 +8,22 @@ from sklearn.metrics import make_scorer, accuracy_score
 import numpy as np
 
 class SpacyTextCatClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, dropout=0.5, max_epochs=10, batch_size=8):
+    def __init__(self, nlp, max_epochs=10, max_steps=None, batch_size=32, dropout=0.2, learn_rate=0.001):
         self.dropout = dropout
         self.max_epochs = max_epochs
         self.batch_size = batch_size
+        self.max_steps = max_steps
+        self.learn_rate = learn_rate
         self.nlp = spacy.blank('en')
         self.textcat = self.nlp.add_pipe('textcat', last=True)
         self.textcat.add_label("POSITIVE")
         self.textcat.add_label("NEGATIVE")
-        self.classes_ = np.array(["NEGATIVE", "POSITIVE"])  # 设置 classes_
+        self.classes_ = np.array(["NEGATIVE", "POSITIVE"])
 
     def fit(self, X, y):
         train_data = [(text, {"cats": {"POSITIVE": label, "NEGATIVE": not label}}) for text, label in zip(X, y)]
         optimizer = self.nlp.begin_training()
+        optimizer.learn_rate = self.learn_rate
         for epoch in range(self.max_epochs):
             losses = {}
             batches = minibatch(train_data, size=self.batch_size)
@@ -50,7 +53,8 @@ y_train = [1, 0, 1, 0]
 param_grid = {
     'classifier__dropout': [0.2, 0.5],
     'classifier__max_epochs': [5, 10],
-    'classifier__batch_size': [4, 8]
+    'classifier__batch_size': [4, 8],
+    'classifier__learn_rate': [0.001, 0.01, 0.1]
 }
 
 # 创建模型管道
@@ -58,7 +62,7 @@ pipe = Pipeline([
     ('classifier', SpacyTextCatClassifier())
 ])
 
-# 使用GridSearchCV进行参数调优，设置cv参数小于样本数量
+# 使用GridSearchCV进行参数调优
 grid_search = GridSearchCV(pipe, param_grid, cv=2, scoring=make_scorer(accuracy_score))
 grid_search.fit(X_train, y_train)
 
